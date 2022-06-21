7e05ba2be177cd2b18a03ac84226d3b6156679b7
peffin <107869895+peffin@users.noreply.github.com>
2022-06-20 22:28:35 -0800

    Add files via upload

---
+++ Multiplication Table.py
@@ -0,0 +1,8 @@
+Y=int(input("Enter the number for multiplcation="))#assume 2
+t=int(input("Enter the lIMIT="))
+X=1
+while X<=t:
+    print(X,"*",Y,"=", X*Y)
+    X=X+1
+else:
+    print("Completed")
